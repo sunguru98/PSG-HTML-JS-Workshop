87849d8dd39db1642b1a73abb66b02a07ff9a988
Sundeep charan Ramkumar <webdevdesign@sundeepcharan.com>
2019-07-27 11:49:19 +0530

    refactoring the code for collison detection

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ let dx = 2
 let dy = -2
 
 // Canvas functions
-const draw = () => {
+const drawCircle = () => {
   console.log('Drawing')
   // Inorder to clear the previous circle, we clear any rectangle
   // inside the whole canvas because we set the boundaries to the full width 
@@ -47,10 +47,21 @@ const draw = () => {
   ctx.fillStyle = '#0095DD'
   ctx.fill()
   ctx.closePath()
+}
+const draw = () => {
+  // Drawing the circle
+  drawCircle()
   // Changing the x and y coords
   x += dx
-  y += dy 
+  y += dy
+  // Collision detection of the ball
+  // If the sum of x and differential x is greater than the difference between the canvas width and the radius of the circle
+  // Change the direction of x
+  // Similarly if the sum of y and differential y is greater than the difference between the canvas height and the radius of the circle
+  // Change the direction of y
+  dx = (x + dx  > canvas.width - 5 || x + dx < 5) ? -dx : dx
+  dy = (y + dy  > canvas.height - 5 || y + dy < 5) ? -dy : dy
 }
 
 // Runs the function for every 10 ms
-setInterval(draw, 100)
+setInterval(draw, 1)
